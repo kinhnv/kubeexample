3c0c64dfd6a6460514919079f2c1d7cbac72e2de
KinhNguyenVan <kinhnv1801@gmail.com>
2019-04-13 00:23:07 +0700

    apply rc

--- Service1/Controllers/ValuesController.cs
+++ Service1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Service1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Service1" };
         }
 
         // GET api/values/5
